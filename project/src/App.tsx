40b1732e4563f906bbbe5b7456934bc6549827d2
wevertoum <wevertonmixalt@gmail.com>
2025-01-15 00:02:02 -0300

    first setup

---
+++ project/src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useNavigate } from "react-router";
+
+const App = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/todo-list");
+  };
+
+  return (
+    <>
+      <p className="text-2xl text-gray-900">Welcome to to-do system</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleClick}
+      >
+        Go to Todo Page
+      </button>
+    </>
+  );
+};
+
+export default App;
