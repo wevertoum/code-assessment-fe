2d7500ff3ef0ed0ae86bd73cb7809364b65a5f4e
wevertoum <wevertonmixalt@gmail.com>
2025-01-15 00:52:16 -0300

    updating use case example jsx test

---
+++ project/src/components/HelloWorld.spec.tsx
@@ -0,0 +1,13 @@
+import "@vitest/browser/matchers.d.ts";
+import { expect, test } from "vitest";
+import { render } from "vitest-browser-react";
+import HelloWorld from "./HelloWorld";
+
+test("renders name", async () => {
+  const { getByText, getByRole } = render(<HelloWorld name="Vitest" />);
+
+  await expect.element(getByText("Hello Vitest x1!")).toBeInTheDocument();
+  await getByRole("button", { name: "Increment " }).click();
+
+  await expect.element(getByText("Hello Vitest x2!")).toBeInTheDocument();
+});
--- project/src/utils/helpers.test.ts
+++
@@ -1,6 +0,0 @@
-import { capitalize } from "./helpers";
-import { it, expect } from "vitest";
-
-it("should return a capitalized string", () => {
-  expect(capitalize("weverton")).toBe("Weverton");
-});
